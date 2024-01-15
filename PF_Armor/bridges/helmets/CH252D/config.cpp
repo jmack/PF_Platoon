2eb34a35eaf87bb99954fd79e6a0331a880b6c97
James Mack <jmack.contact@gmail.com>
2024-01-14 21:58:31 -0600

    Add Bridges custom helmet

---
+++ PF_Armor/bridges/helmets/CH252D/config.cpp
@@ -0,0 +1,37 @@
+class CfgPatches
+{
+  class PF_Armor_Helmets_CH252D_Bridges
+  {
+    author = "wlan0";
+    addonRootClass = "PF_Armor";
+    requiredVersion = 0.1;
+    requiredAddons[] = {};
+    units[] = {};
+    weapons[] = {};
+  };
+};
+
+class CfgWeapons
+{
+  class PF_Armor_Helmets_CH252D_Wizard;
+  class PF_Armor_Helmets_CH252D_Wizard_dp;
+
+  class PF_Armor_Helmets_Bridges : PF_Armor_Helmets_CH252D_Wizard
+  {
+    displayName = "[PF] CH252D (Bridges)";
+    hiddenSelectionsTextures[] =
+    {
+      "PF_Armor\bridges\helmets\CH252D\_textures\black.paa",
+      "PF_Armor\_base\helmets\CH252D\_textures\visr_standard.paa",
+    };
+  };
+  class PF_Armor_Helmets_Bridges_dp : PF_Armor_Helmets_CH252D_Wizard_dp
+  {
+    displayName = "[PF] CH252D Depolarized (Bridges)";
+    hiddenSelectionsTextures[] =
+    {
+      "PF_Armor\bridges\helmets\CH252D\_textures\black.paa",
+      "PF_Armor\_base\helmets\CH252D\_textures\visr_clear.paa",
+    };
+  };
+};
