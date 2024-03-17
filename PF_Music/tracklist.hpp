a15be84ba5adea3a32099a898afe362270223743
OriUnder <90448425+OriUnder@users.noreply.github.com>
2024-03-17 02:43:24 -0400

    adjusted name of class in .hpp, explained formatting

--- PF_Music/tracklist.hpp
+++ PF_Music/tracklist.hpp
@@ -1,7 +1,15 @@
-class aTrackForGuide
+class Pathfinder_Van_Canto // name of class (not really important)
 {
-  name = "Pathfinder - by the crew of the Warlock (Van Canto)";
-  sound[] = {"PF_Music\tracks\Van Canto - Hero - 03 Pathfinder.ogg",db+0,1};
-  duration=286;
-  musicClass = "PF_Music_Tracks";
+  name = "Pathfinder - by the crew of the Warlock (Van Canto)"; // what shows up in game
+  sound[] = {"PF_Music\tracks\Van Canto - Hero - 03 Pathfinder.ogg",db+0,1}; //relative path, volume adjust, and SOMETHING???
+  duration = 286; // time in seconds
+  musicClass = "PF_Music_Tracks"; // points to the config.cpp musicClass
 };
+
+// class Helldivers_2_MainTheme
+// {
+//   name ="Helldivers 2 Main Theme";
+//   sound[] = {"relative path",db+0,1};
+//   duration = 00;
+//   musicClass = "PF_Music_Tracks";
+// };
