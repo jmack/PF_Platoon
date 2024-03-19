f0aef9efca13727d614089b4f5197e64d64a9088
OriUnder <90448425+OriUnder@users.noreply.github.com>
2024-03-19 12:51:56 -0400

    Establishment of PF Music (#29)
    * untested music but hopefully formatted correctly
    
    * adjusted name of class in .hpp, explained formatting
    
    * fixed requested changes

---
+++ PF_Music/tracklist.hpp
@@ -0,0 +1,8 @@
+class Pathfinder_Van_Canto // name of class (not really important)
+{
+  name = "Pathfinder - by the crew of the Warlock (Van Canto)"; // what shows up in game
+  sound[] = {"PF_Music\tracks\Van Canto - Hero - 03 Pathfinder.ogg",db+0,1}; //relative path, volume adjust, and SOMETHING???
+  duration = 286; // time in seconds
+  musicClass = "PF_Music_Tracks"; // points to the config.cpp musicClass
+};
+
