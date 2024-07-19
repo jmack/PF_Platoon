29280c7f2e3cb6251f11e653b228e1631a18274e
jmack <jmack.contact@gmail.com>
2024-07-19 07:00:06 -0500

    Change faction namelist and facelist (#49)
    * Switch heads of BLUFOR and OPFOR american forces
    
    * Create custom name lists for american factions

---
+++ NASR/NASR_Factions/_names/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches
+{
+  class NASR_Factions_Names
+  {
+    author = "wlan0";
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    addonRootClass = "NASR_Factions";
+  };
+};
