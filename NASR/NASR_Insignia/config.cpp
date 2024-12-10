6842e286f99c85b333de0b47c8d0533e5154698c
James Mack <jmack.contact@gmail.com>
2024-12-10 09:24:38 -0600

    Add ED, ED Subdued, and PRA patches

--- NASR/NASR_Insignia/config.cpp
+++ NASR/NASR_Insignia/config.cpp
@@ -38,6 +38,15 @@ class CfgUnitInsignia
     textureVehicle = "NASR_Insignia\data\argentina.paa";
   };
 
+  class NASR_Insignia_Peoples_Revolutionary_Army
+  {
+    dlc = "NASR";
+    displayName = "[PF] People's Revolutionary Army";
+    author = "wlan0";
+    texture = "NASR_Insignia\data\red_argentina.paa";
+    textureVehicle = "NASR_Insignia\data\red_argentina.paa";
+  };
+
   class NASR_Insignia_Spain
   {
     dlc = "NASR";
@@ -45,5 +54,23 @@ class CfgUnitInsignia
     author = "wlan0";
     texture = "NASR_Insignia\data\spain.paa";
     textureVehicle = "NASR_Insignia\data\spain.paa";
+  };
+
+  class NASR_Insignia_English_Directorate
+  {
+    dlc = "NASR";
+    displayName = "[PF] English Directorate";
+    author = "wlan0";
+    texture = "NASR_Insignia\data\english_directorate.paa";
+    textureVehicle = "NASR_Insignia\data\english_directorate.paa";
+  };
+
+  class NASR_Insignia_English_Directorate_Subdued_Woodland
+  {
+    dlc = "NASR";
+    displayName = "[PF] English Directorate (Subdued, Woodland)";
+    author = "wlan0";
+    texture = "NASR_Insignia\data\english_directorate_subdued_woodland.paa";
+    textureVehicle = "NASR_Insignia\data\english_directorate_subdued_woodland.paa";
   };
 };
