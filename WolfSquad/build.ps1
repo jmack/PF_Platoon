d05da13d7f77c8012c56206317d4627fe8117332
James Mack <jmack.contact@gmail.com>
2025-06-01 09:54:39 -0500

    Initial Commit

---
+++ WolfSquad/build.ps1
@@ -0,0 +1,30 @@
+# Create addons folder if it doesn't exist
+$addonsDirPath = "" + $( Get-Location ) + "\dist\addons";
+if (!(Test-Path -PathType container $addonsDirPath)) {
+	New-Item -ItemType Directory -Path $addonsDirPath
+}
+
+. ../vars.ps1
+
+# Preprocessor
+# Node.js check (for tic-tac generation)
+# if (Get-Command node -errorAction SilentlyContinue) {
+# 	node .\PF_Gear\tictacs\tictac-converter.js
+# } else {
+# 	Write-Host "[WARN] Node not installed in this environment. Skipping tic-tac generation" -ForegroundColor Yellow
+# }
+
+# Build
+if ($args.count -gt 0) {
+	$dirs = $args[0];
+} else {
+	$dirs = "WS_*";
+}
+
+Get-ChildItem . -Directory $dirs | Foreach-Object {
+	$fn = $_.FullName;
+	$signArg = "-sign=" + $privKey;
+	$whitelistArg = "-include=" + $( Get-Location ) + "\..\addonBuilderWhitelist.txt";
+	
+	& $addonBuilder $fn $destination $signArg $whitelistArg -binarizeFullLogs -binarizeAllTextures -clear;
+}
