ec9450dc54cc0fa8c8e736b2591d272b8d0fb233
James Mack <jmack.contact@gmail.com>
2025-09-25 12:57:07 -0500

    Move requirements into root WS_Gear module

--- WolfSquad/WS_Gear/config.cpp
+++ WolfSquad/WS_Gear/config.cpp
@@ -6,5 +6,11 @@ class CfgPatches
     units[] = {};
     weapons[] = {};
     requiredVersion = 0.1;
+    requiredAddons[] =
+    {
+      "MA_Armor",
+      "OPTRE_ACE_Compat",
+      "OPTRE_FC_ACE_Compat",
+    };
   };
 };
