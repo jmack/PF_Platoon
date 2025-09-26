4f0cbaa4a200b7b8ac4c0a1a8561d9dff2abad52
James Mack <jmack.contact@gmail.com>
2025-09-25 19:41:47 -0500

    Update script to no longer need renaming of pngs to avoid binarization

--- WolfSquad/build.ps1
+++ WolfSquad/build.ps1
@@ -6,6 +6,25 @@ if (!(Test-Path -PathType container $addonsDirPath)) {
 
 . ../vars.ps1
 
+# Find all image files, check if they've been edited (via git), and if not, change their extension to avoid binarization
+$allPngs = @();
+$modifiedPngs = @();
+
+if ($null -ne (git ls-files "WS_*/*/*.png")) {
+	$allPngs = (git ls-files "WS_*/*/*.png").Split([Environment]::NewLine);
+}
+if ($null -ne (git ls-files -mo "WS_*/*/*.png")) {
+	$modifiedPngs = (git ls-files -mo "WS_*/*/*.png").Split([Environment]::NewLine);
+}
+
+foreach ($item in $allPngs) {
+	if ($item -notin $modifiedPngs) {
+		$strSplit = $item.Split("/");
+		$name = $strSplit[$strSplit.Count - 1];
+		Rename-Item -Path "$item" -NewName "$name.excluded";
+	}
+}
+
 # Build
 if ($args.count -gt 0) {
 	$dirs = $args[0];
@@ -19,4 +38,15 @@ Get-ChildItem . -Directory $dirs | Foreach-Object {
 	$whitelistArg = "-include=" + $( Get-Location ) + "\..\addonBuilderWhitelist.txt";
 	
 	& $addonBuilder $fn $destination $signArg $whitelistArg -binarizeFullLogs -binarizeAllTextures -clear;
+}
+
+# Change back all images we named .excluded above to just .png
+$excludedPngs = @();
+if ($null -ne (git ls-files -mo "WS_*/*/*.excluded")) {
+	$excludedPngs = (git ls-files -mo "WS_*/*/*.excluded").Split([Environment]::NewLine);
+}
+foreach ($item in $excludedPngs) {
+	$strSplit = $item.Split("/");
+	$name = $strSplit[$strSplit.Count - 1].Replace(".excluded", "");
+	Rename-Item -Path "$item" -NewName "$name";
 }
