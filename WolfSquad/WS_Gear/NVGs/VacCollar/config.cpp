5a248fb2eb5e0024dad27b33bb5ee2960af210b3
James Mack <jmack.contact@gmail.com>
2025-09-27 19:56:52 -0500

    Add vacseal and eod cbrn NVGs

---
+++ WolfSquad/WS_Gear/NVGs/VacCollar/config.cpp
@@ -0,0 +1,33 @@
+class CfgPatches
+{
+  class WS_Gear_NVG_VacCollar
+  {
+    author = "wlan0";
+    addonRootClass = "WS_Gear";
+  };
+};
+
+class CfgWeapons
+{
+  // Vacuum Seal
+  class VES_NVG_Collar_VAC;
+  class WS_Gear_NVG_VacCollar : VES_NVG_Collar_VAC
+  {
+    dlc = "WS";
+    author = "wlan0";
+    displayName = "[WS] Vacuum Seal Collar";
+    picture = "WS_Images\Common\logo_color.paa";
+    visionMode[] =
+    {
+      "Normal",
+      "NVG",
+      "TI",
+    };
+    thermalMode[] = {0};
+    ace_nightvision_generation = 4;
+    ace_nightvision_bluRadius = 0.13;
+    ace_nightvision_eyeCups = 0;
+    ace_nightvision_colorPreset[] = { 0, { 0, 0, 0, 0 }, { 1.3, 1.2, 0, 0.9 }, { 6, 1, 1, 0 }}; // Standard green
+    ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_hexes_thin.paa";
+  };
+};
